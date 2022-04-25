644a153269ceb7163058465c4e62d243d7336efb
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2022-04-25 14:47:18 +0200

    Add Round robin scheduler

--- ProcessorScheduler/CPUSystemRoundRobin.cpp
+++ ProcessorScheduler/CPUSystemRoundRobin.cpp
@@ -1,1 +1,22 @@
 #include "CPUSystemRoundRobin.hpp"
+
+CPUSystemRoundRobin::CPUSystemRoundRobin(CPU** cpus, int cpuCount) : CPUSystemAbstract(cpus, cpuCount)
+{ }
+
+
+
+void CPUSystemRoundRobin::Schedule(queue<char> tasks)
+{
+    CPU** cpus;
+    int cpuCount;
+    GetCPUs(cpus, cpuCount);
+
+    int selectedCPU = 0;
+    while(!tasks.empty())
+    {
+        cpus[selectedCPU]->AddTask(tasks.front());
+        tasks.pop();
+        
+        selectedCPU = (selectedCPU + 1) % cpuCount;
+    }
+}
--- ProcessorScheduler/CPUSystemRoundRobin.hpp
+++ ProcessorScheduler/CPUSystemRoundRobin.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <queue>
+#include <iostream>
+
 #include "CPUSystemAbstract.hpp"
+#include "CPU.hpp"
 
 class CPUSystemRoundRobin : CPUSystemAbstract
 {
+protected:
+	virtual void Schedule(queue<char> tasks) override;
+
+public:
+	CPUSystemRoundRobin(CPU** cpus, int cpuCount);
 };
 
