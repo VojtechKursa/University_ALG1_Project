b59cd8ae355d14b933bd634d8750789061712e24
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2022-04-27 10:32:11 +0200

    Complete basic implementation

--- ProcessorScheduler/main.cpp
+++ ProcessorScheduler/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <queue>
 
 #include "CommandLineArguments.hpp"
@@ -13,55 +14,156 @@
 
 using namespace std;
 
-int random(int min, int max)
+void printHelp()
 {
-	return (rand() % (max - min)) + min;
+	// To do: help
 }
 
-int main(int argc, char** argv)
+CPUSystemAbstract* createCPUSystem(CommandLineArguments args)
 {
-	srand((unsigned int)time(NULL));
+	const int cpuCount = 3;
 
-	int batchNum = 4;
-	queue<char> taskBatch;
+	CPUSystemAbstract* cpuSystem;
 
-	CPU* cpus[3];
-	cpus[0] = new CPU("CPU L", 'L');
-	cpus[1] = new CPU("CPU M", 'M');
-	cpus[2] = new CPU("CPU H", 'H');
+	CPU* cpus[cpuCount];
+	cpus[0] = new CPU("Cortex-A510 (L)", 'L');
+	cpus[1] = new CPU("Cortex-A710 (M)", 'M');
+	cpus[2] = new CPU("Cortex-X2 (H)", 'H');
 
-	CPUSystemAbstract* cpuSystem = new CPUSystemSimple(cpus, 3);
+	switch (args.GetSelectedScheduler())
+	{
+	case SelectedScheduler::RoundRobin: cpuSystem = new CPUSystemRoundRobin(cpus, cpuCount); break;
+	case SelectedScheduler::Simple:
+	case SelectedScheduler::Unspecified:
+	default: cpuSystem = new CPUSystemSimple(cpus, cpuCount); break;
+	}
 
 	for (int i = 0; i < 3; i++)
 	{
 		delete cpus[i];
+		cpus[i] = nullptr;
 	}
 
-	for (int currBatch = 0; currBatch < 4; currBatch++)
+	return cpuSystem;
+}
+
+ifstream openInputFile(CommandLineArguments args)
+{
+	ifstream inputFile;
+
+	if (args.GetInputFile() != "")
 	{
-		cout << "Batch " << currBatch << ": ";
+		inputFile.open(args.GetInputFile(), ios::in);
 
-		for (int i = 0; i < 8; i++)
+		if (!inputFile.is_open())
 		{
-			int taskNum = random(0, 2);
-			char task;
+			cout << "ERR: Failed to open the input file " << args.GetInputFile() << ".\n";
+			exit(1);
+		}
+	}
+	else
+	{
+		cout << "ERR: Input file not specified.\n";
+		exit(1);
+	}
 
-			switch (taskNum)
-			{
-			case 0: task = 'L'; break;
-			case 1:	task = 'M'; break;
-			case 2: task = 'H'; break;
-			default: task = 0; break;
-			}
+	return inputFile;
+}
 
-			cout << task;
-			taskBatch.push(task);
+ofstream openOutputFile(CommandLineArguments args)
+{
+	ofstream outputFile;
+
+	if (args.GetOutputFile() != "")
+	{
+		outputFile.open(args.GetOutputFile(), ios::out);
+
+		if (!outputFile.is_open())
+		{
+			cout << "ERR: Failed to open the output file " << args.GetOutputFile() << ".\n";
+			exit(1);
+		}
+	}
+
+	return outputFile;
+}
+
+int main(int argc, char** argv)
+{
+	CommandLineArguments args(argc, argv);
+
+	if (args.GetPrintHelp())
+	{
+		printHelp();
+		exit(0);
+	}
+
+	ifstream inputFile = openInputFile(args);
+	
+	CPUSystemAbstract* cpuSystem = createCPUSystem(args);
+
+	string buffer;
+	getline(inputFile, buffer);
+	if (inputFile.fail())
+	{
+		cout << "ERR: Failed to read line 1 of input file.\n";
+		exit(1);
+	}
+	int taskCount;
+	try
+	{
+		taskCount = stoi(buffer);
+	}
+	catch (exception)
+	{
+		cout << "ERR: First line of the input file must be a number.\n";
+		exit(1);
+	}
+
+	queue<char> allTasks;
+
+	for(int i = 0; i < taskCount; i++)
+	{
+		getline(inputFile, buffer);
+
+		if (inputFile.fail() || buffer.size() < 1)
+		{
+			cout << "ERR: Failed to read line " << i + 1 << " of input file.\n";
+			exit(1);
+		}
+
+		allTasks.push(buffer[0]);
+	}
+
+	inputFile.close();
+
+	ofstream outputFile = openOutputFile(args);
+	queue<char> taskBatch;
+
+	while (!allTasks.empty())
+	{
+		int stopPoint = min(8, (int)allTasks.size());
+		for (int i = 0; i < stopPoint; i++)
+		{
+			taskBatch.push(allTasks.front());
+			allTasks.pop();
 		}
 
 		cpuSystem->ProcessBatch(taskBatch);
 
-		cout << endl << cpuSystem->GetBatchReport() << endl << endl;
+		if (outputFile.is_open())
+			outputFile << cpuSystem->GetBatchReport() << "\n\n";
+		if(args.GetAllowCOut())
+			cout << cpuSystem->GetBatchReport() << "\n\n";
 	}
+
+	cout << "Simulation finished, total cycles worked: " << cpuSystem->GetCyclesWorkedTotal() << endl;
+
+	if(outputFile.is_open())
+		outputFile << "Simulation finished, total cycles worked: " << cpuSystem->GetCyclesWorkedTotal() << endl;
+
+	outputFile.close();
+
 
 	delete cpuSystem;
 
